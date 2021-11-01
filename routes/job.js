ec546577ed1a4cc9d22fe46a72640d30ec15db8e
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-11-01 11:48:34 -0500

    job stuff

--- routes/job.js
+++ routes/job.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Seth Bennett' });
+  res.render('job', { title: 'Search Results Job' });
 });
 
 module.exports = router;

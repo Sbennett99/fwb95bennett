a2cd218de8c5c5b100ce8d3f664ff969ea61bfbd
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-11-03 18:29:34 -0500

    addMods stuff

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jobRouter = require('./routes/job');
+var addModsRouter = require('./routes/addMods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/job', jobRouter);
+app.use('/addMods', addModsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
